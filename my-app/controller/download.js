3e31aa17b9c6f905b1954116335ec26ec8653907
グレートサイヤマン8号 <105696434+nnhrBelievers@users.noreply.github.com>
2022-07-19 12:30:05 +0900

    Add files via upload

---
+++ my-app/controller/download.js
@@ -0,0 +1,12 @@
+const { S3, GetObjectCommand } = require('@aws-sdk/client-s3')
+
+module.exports = function(req, res) {
+  const client = new S3()
+  const fileName = req.query.name
+
+  client.send(new GetObjectCommand({ Bucket: 'cupper-hj-test', Key: fileName }))
+    .then(data => {
+      res.attachment(fileName)
+      data.Body.pipe(res)
+    })
+}
